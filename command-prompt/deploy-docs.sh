4a1b6d653079af0fa46bc8395deb5555da3fdbc1
Andrei Jiroh Halili <andreijiroheugeniohalili24680@gmail.com>
2019-09-30 06:42:29 +0000

    Update .gitlab-ci.yml, command-prompt/deploy-docs.sh files

---
+++ command-prompt/deploy-docs.sh
@@ -0,0 +1,9 @@
+# Install MkDocs first
+pip install mkdocs
+
+# Then, install the themes we need for our documentation
+pip install mkdocs-material
+
+# Last, install the plugins we're required.
+pip install mkdocs-redirects
+pip install mkdocs-macros-plugin
